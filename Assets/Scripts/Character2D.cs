2c86142ecabe2e0abd85f984ec126740c6597a2b
Marco Antonio Rodríguez <marcosrodso@gmail.com>
2020-02-16 19:54:48 -0700

    inicio

---
+++ Assets/Scripts/Character2D.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character2D : MonoBehaviour
+{
+    [SerializeField]
+    float moveSpeed;
+    //float moveSpeed {get; set;}
+
+    void Update()
+    {
+        transform.Translate(Axis * moveSpeed * Time.deltaTime);
+    }
+
+    Vector2 Axis
+    {
+        get => new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    }
+
+    public float MoveSpeed { get => moveSpeed; set => moveSpeed = value; }
+}
